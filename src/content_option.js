b6a650ddc21aadb41f8664cbff015d88818fa9c3
arslaanKhurshid <arslaankhurshiedsmt231@gmail.com>
2024-11-01 01:56:15 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+import profileImage from './images/me3.jpg';
+const logotext = "ZAZ ARSALAN KHURSHEED ";
+const meta = {
+    title: "Zaz Arsalan",
+    description: "I’m Zaz Arsalan Comp-Science major ,currently working in Bangalore",
+};
+
+const introdata = {
+    title: "Hey, I’m Zaz Arsalan",
+    animated: {
+        first: "I am a Software Developer",
+        second: "I love DSA",
+        third: "I learning development rn",
+    },
+    description: "I am Zaz Arsalan Khursheed, a Computer Science Engineering student with good experience in OOPS, DSA, Web-Dev, AI/ML. I possess strong programming and database skills and am actively involved in projects and extracurricular activities.",
+    your_img_url: profileImage,
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I am Zaz Arsalan Khursheed, a Computer Science Engineering student with good experience in OOPS, DSA, Web-Dev, AI/ML. I possess strong programming and database skills and am actively involved in projects and extracurricular activities.",
+};
+const worktimeline = [{
+        jobtitle: "Student at Ramaiah Institute of Technology.",
+        where: "Bangalore,India",
+        date: "2024",
+    }
+];
+
+const skills = [
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 65,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    
+    {
+        name: "Java",
+        value: 95, // Adjust the value as needed
+    },
+    {
+        name: "C",
+        value: 75, // Adjust the value as needed
+    },
+    {
+        name: "C++",
+        value: 50, // Adjust the value as needed
+    },
+    {
+        name: "HTML",
+        value: 90, // Adjust the value as needed
+    },
+    {
+        name: "CSS",
+        value: 85, // Adjust the value as needed
+    },
+    {
+        name: "MySQL",
+        value: 90, // Adjust the value as needed
+    },
+    {
+        name: "MongoDB",
+        value: 70, // Adjust the value as needed
+    },
+    {
+        name: "GitHub",
+        value: 80, // Adjust the value as needed
+    },
+    {
+        name: "Git",
+        value: 80, // Adjust the value as needed
+    },
+    {
+        name: "Jenkins",
+        value: 20, // Adjust the value as needed
+    },
+    {
+        name: "Docker",
+        value: 20, // Adjust the value as needed
+    },
+    {
+        name: "VSCode",
+        value: 80, // Adjust the value as needed
+    },
+    {
+        name: "Neovim",
+        value: 60, // Adjust the value as needed
+    },
+];
+
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "StudyBuddy is a study chat room where you can interact with your peers and learn.",
+        link: "https://github.com/arslaanKhurshid/study_buddy",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Crowdfunding made secure with Secure-Flow.",
+        link: "https://github.com/arslaanKhurshid/SecureFlow.git",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A simple react stop watch.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "A react To-Do list front end app.",
+        link: "https://github.com/arslaanKhurshid/ToDo-List.git",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "arslaankhurshiedsmt231@gmail.com",
+    YOUR_FONE: "(+91)9797104302",
+    description: "Use the email/phone number to get in touch you can approach me on my socials.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_xs072dl",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/arslaanKhurshid",
+    instagram: "https://www.instagram.com/arslaan_khurshied/",
+    linkedin: "https://www.linkedin.com/in/zaz-arsalan-khursheed-aa3612238?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BPIA5zkuOTUi3olJyN%2FxqfA%3D%3D",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
